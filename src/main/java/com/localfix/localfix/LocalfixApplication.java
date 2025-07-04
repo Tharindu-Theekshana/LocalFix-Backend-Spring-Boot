6b142db15854bb0fee30f94729c6c08f959762bb
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-07-04 16:22:27 +0530

    added related dependencies

---
+++ src/main/java/com/localfix/localfix/LocalfixApplication.java
@@ -0,0 +1,15 @@
+package com.localfix.localfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalfixApplication.class, args);
+
+	}
+
+
+}
---
+++ src/test/java/com/localfix/localfix/LocalfixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localfix.localfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalfixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
