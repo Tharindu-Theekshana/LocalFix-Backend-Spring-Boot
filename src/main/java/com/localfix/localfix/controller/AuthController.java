bedbf97f5176a667d46252b181d50d361d2bd27c
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-07-05 15:33:17 +0530

    Update AuthController.java

--- src/main/java/com/localfix/localfix/controller/AuthController.java
+++ src/main/java/com/localfix/localfix/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.localfix.localfix.service.AuthSerice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class AuthController {
     AuthSerice authSerice;
 
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(UserDto userDto){
+    public ResponseEntity<AuthResponse> register(@RequestBody UserDto userDto){
 
         AuthResponse response = authSerice.register(userDto);
         return ResponseEntity.ok(response);
