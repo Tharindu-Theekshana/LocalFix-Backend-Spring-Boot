3f47897a17f0b098d327f89ad640089955495896
Tharindu-Theekshana <tariduthikshana4@gmail.com>
2025-07-06 18:35:41 +0530

    custom user details service implemented

--- src/main/java/com/localfix/localfix/repository/UserRepo.java
+++ src/main/java/com/localfix/localfix/repository/UserRepo.java
@@ -3,6 +3,11 @@ package com.localfix.localfix.repository;
 import com.localfix.localfix.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Integer> {
     boolean existsByEmail(String email);
+
+
+    Optional<User> findByEmail(String email);
 }
